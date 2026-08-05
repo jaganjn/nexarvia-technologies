00107d720556d1a26b23076fe903b833866ea251
Nagaraj <nagaraj2452000@gmail.com>
2026-08-05 17:37:09 +0530

    done

--- site-config.js
+++ site-config.js
@@ -1,17 +1,6 @@
 window.NEXARVIA_SITE_CONFIG = {
-  supportEmail: "your-email@example.com",
-
-  technologyInquirySheetEndpoint:
-    "https://script.google.com/macros/s/AKfycbwiJn1WkP-3xox6j0f0MRHm_KU6ovrdELlokPcXxZ-EqF0tAUitHV7tJvE2X2-bzubr/exec",
-
-  technologyInquirySheetName:
-    "Nexarvia Technology Services Enquiries",
-
-  social: {
-    linkedin: "",
-    instagram: "",
-    youtube: "",
-    facebook: "",
-    whatsapp: ""
-  }
+  supportEmail: "",
+  technologyInquirySheetEndpoint: "https://script.google.com/macros/s/AKfycbwiJn1WkP-3xox6j0f0MRHm_KU6ovrdELlokPcXxZ-EqF0tAUitHV7tJvE2X2-bzubr/exec",
+  technologyInquirySheetName: "Nexarvia Technology Services Enquiries",
+  social: { linkedin: "", instagram: "", youtube: "", facebook: "", whatsapp: "" }
 };
